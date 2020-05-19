86efc5bc3d367f0d2fe757d028708dd850c6177a
Daggy1234 <60603110+Daggy1234@users.noreply.github.com>
2020-05-19 12:30:35 +0530

    made da bot

--- bot.py
+++ bot.py
@@ -3,14 +3,64 @@ from discord.ext import commands
 import yaml
 import asyncio
 import traceback
+import os
+import random
+from io import BytesIO
+import praw
+from django.core.validators import URLValidator
+from django.core.exceptions import ValidationError
+import aiohttp
+from PIL import Image,ImageFilter
 from async_timeout import timeout
 bot = commands.Bot(command_prefix = commands.when_mentioned_or('.'))
 bot.currentgames = []
 bot.gamecount = 0
 with open(r'data.yml') as file:
     tokend = yaml.load(file,Loader=yaml.FullLoader)
-    token = tokend['token']
+    bot.token = tokend['token']
+    bot.secret = tokend['secret']
+    bot.id = tokend['id']
+    bot.username = tokend['username']
+    bot.password = tokend['password']
     file.close()
+async def getbyt(url):
+    async with aiohttp.ClientSession() as session:
+            async with session.get(url) as r:
+                if r.status == 200:
+                    byt = (await r.read())
+                    return(byt)
+                    #await imgf.close()
+                else:
+                    return False
+                del r
+def postmeme(byt):
+    reddit = praw.Reddit(client_id = bot.id,
+                         client_secret = bot.secret,
+                         user_agent = bot.id,
+                         username = bot.username,
+                         password = bot.password)
+    with Image.open(BytesIO(byt)) as img:
+        size = img.size
+        wmark = Image.open('watermark.png')
+        wthf = wmark.resize((100,100),5)
+        wthf.putalpha(64)
+        y = size[1]-100
+        img.paste(wthf,(0,y),wthf)
+        img.save('meme.png')
+        image = 'meme.png'
+        title = 'This is a test of the uploading feature!'
+        sub = reddit.subreddit('MemeMixGame').submit_image(title,image,timeout=30.0)
+        y = sub.permalink
+        furls = f'https://www.reddit.com{y}'
+        return(furls)
+        os.remove('meme.png')
+        
+
+    
+
+    
+
+
 class MyHelpCommand(commands.MinimalHelpCommand):
     def get_command_signature(self, command):
         return '{0.clean_prefix}{1.qualified_name} {1.signature}'.format(self, command)
@@ -47,11 +97,11 @@ class mememix(commands.Cog):
                     while True:
                         try:
                             reaction,user = await self.bot.wait_for('reaction_add',timeout=2.0,check=votecheck)
-                            if (str(reaction.emoji) in ['\U000026a0','\U0000274e']):
+                            if (str(reaction.emoji) in ['<:upvote:700689655607197746>','\U0000274e']):
                                 if str(reaction.emoji) == '\U0000274e':
                                     if user.guild_permissions.manage_guild:
                                         embed = discord.Embed(title='Memix Results!')
-                                        channel = self.bot.get_channel(711890872974835763)
+                                        channel = self.bot.get_channel(710470130680987648)
                                         winndex = 0
                                         ma = -999999999999999999999999999999999999999999999
                                         for i in range(0,3):
@@ -69,11 +119,18 @@ class mememix(commands.Cog):
                                         Meme 3: {votes[2]}
                                         The winner is Meme {winndex+1}''')
                                         await channel.send(embed=embed)
-                                        return await channel.send(f'{self.mixer[winndex].mention} COngrats on the win')
-                                        break
+                                        await channel.send(f'{self.mixer[winndex].mention} COngrats on the win')
+                                        msg = await channel.fetch_message(mis[winndex])
+                                        e = msg.embeds[0]
+                                        url = e.image 
+                                        async with ctx.typing():
+                                            byt = await getbyt(url)
+                                            ur = postmeme(byt)
+                                            return await ctx.send(ur)
+                                            break
                                     else:
                                         await reaction.message.remove_reaction(reaction,user)
-                                elif str(reaction.emoji) == '\U000026a0':
+                                elif str(reaction.emoji) == '<:upvote:700689655607197746>':
                                     if user not in vlist:
                                         v = mis.index(reaction.message.id)
                                         votes[v] = votes[v] + 1
@@ -97,7 +154,7 @@ class mememix(commands.Cog):
                             continue
 
             except asyncio.TimeoutError:
-                channel = self.bot.get_channel(711890872974835763)
+                channel = self.bot.get_channel(710470130680987648)
                 embed = discord.Embed(title='Memix Results!')
                 winndex = 0
                 ma = -999999999999999999999999999999999999999999999
@@ -116,14 +173,23 @@ class mememix(commands.Cog):
     Meme 3: {votes[2]}
     The winner is Meme {winndex+1}''')
                 await channel.send(embed=embed)
-                return await channel.send(f'{self.mixer[winndex].mention} COngrats on the win')
+                await channel.send(f'{self.mixer[winndex].mention} COngrats on the win')
+                msg = await channel.fetch_message(mis[winndex])
+                e = msg.embeds[0]
+                url = e.image
+                async with ctx.typing(): 
+                    byt = await getbyt(url)
+                    ur = postmeme(byt)
+                    return await ctx.send(ur)
+                
         else:
             print('Game was unsuccesful')
     @commands.command()
     @commands.is_owner()
     async def voting(self,ctx):
         """ tests the voting system (only the owner can run it) Please do not run this command"""
-        channel = self.bot.get_channel(711890872974835763)
+        channel = self.bot.get_channel(710470130680987648)
+        self.mlist = []
         memeurllist = ['https://media.discordapp.net/attachments/711563666943639582/711897308836397076/un82ifsnvge31.png','https://media.discordapp.net/attachments/711889053192290305/711897237629698138/images_16.jpeg','https://media.discordapp.net/attachments/711556060376465478/711896804324802560/blurple.png']
         await ctx.send('All memes have been submitted')
         for i in range(0,3):
@@ -131,7 +197,7 @@ class mememix(commands.Cog):
             embed.set_image(url=memeurllist[i])
             msg = await channel.send(embed=embed)
             self.mlist.append(msg)
-            await msg.add_reaction('\U000026a0')
+            await msg.add_reaction('<:upvote:700689655607197746>')
         self.commandexec = True
                     
     @commands.command()
@@ -208,7 +274,7 @@ class mememix(commands.Cog):
                         embed = discord.Embed(title=f'Mixer Cycle {self.bot.gamecount+1}: Round {turns+1}')
                         embed.description = (f'''Caption: {c.mention}\nTemplate: {t.mention}\nMixer: {m.mention}\n Please send your captions and template to the Mixer.
 Mixer, please DM your meme to the  <@711503516778233886> , me. Please make sure to use the keyword `submit` while submitting.''')
-                        await ctx.send(embed=embed)
+                        await ctx.send(f'{c.mention},{t.mention},{m.mention}',embed=embed)
                         try:
                             async with timeout(1800.0):
                                 while True:
@@ -222,6 +288,9 @@ Mixer, please DM your meme to the  <@711503516778233886> , me. Please make sure 
                                                     image_url = msg.attachments[0].url
                                                     memeurllist.append(image_url)
                                                     await self.mixer[turns].send('Recieved your submission!')
+                                                    embed = discord.Embed(title = f'{self.mixer[turns].display_name} has submitted their meme')
+                                                    embed.set_image(url=image_url)
+                                                    await ctx.send(embed=embed)
                                                     break
                                                 except:
                                                     await self.mixer[turns].send('I was unable to use the attachment you provided')
@@ -242,17 +311,17 @@ Mixer, please DM your meme to the  <@711503516778233886> , me. Please make sure 
                 return await ctx.send('Mixer cancelled')
             else:
                 peopleuvoted = []
-                channel = self.bot.get_channel(711890872974835763)
+                channel = self.bot.get_channel(710470130680987648)
 
                 
                 await ctx.send('All memes have been submitted')
                 await channel.send(f'Memeix Cycle {self.bot.gamecount+1}')
                 for i in range(0,3):
-                    embed = discord.Embed(title = f'{self.mixer[i].display_name} has submitted their meme')
+                    embed = discord.Embed(title = f'{self.mixer[i].mention} has submitted their meme')
                     embed.set_image(url=memeurllist[i])
                     msg = await channel.send(embed=embed)
                     self.mlist.append(msg)
-                    await msg.add_reaction('\U000026a0')
+                    await msg.add_reaction('<:upvote:700689655607197746>')
                 self.commandexec = True
                 self.bot.gamecount+=1    
 
@@ -260,6 +329,33 @@ Mixer, please DM your meme to the  <@711503516778233886> , me. Please make sure 
 async def on_ready():
     bot.add_cog(mememix(bot))
     print('Bit is onlibe')
+@bot.command()
+async def testcustomemoji(ctx):
+    msg = await ctx.send('will try adding the Uv to this')
+    await msg.add_reaction('<:upvote:700689655607197746>')
+@bot.command()
+@commands.is_owner()
+async def upload(ctx,text:str='Optional'):
+    async with ctx.typing():
+        if len(ctx.message.attachments ) != 0:
+        
+                try:
+                    image_url = ctx.message.attachments[0].url
+                    
+                except:
+                    return await ctx.send('I was unable to use the attachment you provided')
+        elif text != 'Optional':
+            validate = URLValidator()
+            try:
+                validate(text)
+            except ValidationError:
+                return await ctx.send('Your Url is invalid')
+            else:
+                image_url = str(text)
+        print(image_url)
+        byt = await getbyt(image_url)
+        ur = postmeme(byt)
+        return await ctx.send(ur)
 
 @bot.event
 async def on_command_error(ctx,error):
@@ -285,12 +381,13 @@ async def on_command_error(ctx,error):
     elif isinstance(error,commands.CommandNotFound):
         return await ctx.send('That command does not exist')
     else:
+        print(traceback_text)
         name = ctx.author.display_name
         server = ctx.guild.name
         embed = discord.Embed(title='MIXER UNKOWN ERROR OCCURED',description=f'```python\n{error}\n ```\nThe command {ctx.invoked_with} caused the error\n**Author:**{name}\n**Server:**{server}',color=ctx.guild.me.color)
         embed.add_field(name='SENT', value="Your error has been sent to my creator's bug channel")
         nemb =  discord.Embed(title='MIXER UNKOWN ERROR OCCURED',description=f'```python\n{traceback_text}\n ```\nThe command {ctx.invoked_with} caused the error\n**Author:**{name}\n**Server:**{server}',color=ctx.guild.me.color)
-        channel = bot.get_channel(709828603101315157)
+        channel = bot.get_channel(711925873435213846)
         await channel.send(embed=nemb)
         await ctx.send(embed=embed)
 
@@ -305,4 +402,4 @@ async def on_command_error(ctx,error):
 
 
 
-bot.run(token)
+bot.run(bot.token)
